e121d09e0cff99ee46f39ea8cab02293321b2e3d
Kristijan Karanfiloski <kikaccc29@gmail.com>
2023-09-28 16:27:36 +0200

    'destructured the props in card'

--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -3,34 +3,33 @@ import "./Card.scss";
 ////////////////////////////////////////////////////////////////
 // REUSABLE CHILD CARD
 
-const Card = (props) => {
-  console.log(props);
+const Card = ({ title, image, bookmarked, year, category, rating, svg }) => {
+  // Now I can directly use `title`, `image`, etc.
+  // No need to write `props.title`, `props.image`, etc.
   return (
     <>
-      <div key={props.title} className="card">
+      <div key={title} className="card">
         <div className="card__image-wrapper">
           <img
             className="card__image"
-            src={props.image}
+            src={image}
             // src={movie.thumbnail}
             alt="Movie Poster"
           />
           <div className="icon">
-            <img src={props.bookmarked} alt="bookmark icon" />
+            <img src={bookmarked} alt="bookmark icon" />
             {/*{movie.isBookmarked ? <BookmarkFull /> : <BookmarkEmpty />}*/}
           </div>
         </div>
         <div className="card__info">
-          <div className="card__detail card__detail--year">{props.year}</div>
+          <div className="card__detail card__detail--year">{year}</div>
           <div className="card__detail card__detail--category">
-            {props.svg}
-            {props.category}
+            {svg}
+            {category}
           </div>
-          <div className="card__detail card__detail--rating">
-            {props.rating}
-          </div>
+          <div className="card__detail card__detail--rating">{rating}</div>
         </div>
-        <p className="card__name">{props.title}</p>
+        <p className="card__name">{title}</p>
       </div>
     </>
   );
