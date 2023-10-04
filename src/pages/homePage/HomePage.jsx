6f18aef2923fa171edba53578b6ec760b3000cc9
Kristijan Karanfiloski <kikaccc29@gmail.com>
2023-10-04 09:13:34 +0200

    "img path in json fixed"

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -48,6 +48,7 @@ const HomePage = () => {
               <img
                 className="trending__wrapper__container__card__image"
                 src={trendingItemCard.thumbnail.regular.small}
+                // src={trendingItemCard.regular.small}
                 alt="trending img"
               />
 
